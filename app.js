bab29ba8d437756323e6335eb51c147ddef8f43c
MikixIT <michael.46@live.it>
2023-03-07 20:58:21 +0100

    declaring variable (const) link to dom

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("works");
+const cards = document.querySelectorAll('.card');
+const namePoke = document.querySelector('.name');
+const idPoke = document.querySelector('.id');
+const infoPoke = document.querySelector('.pokemon-info');
+const searchValue = document.querySelector('#pokemon-name');
+const buttonSearch = document.querySelector('button');
