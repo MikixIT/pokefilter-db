e045f3b784a31d0985edf4726f569288187a01ed
MikixIT <michael.46@live.it>
2023-03-08 19:45:38 +0100

    rebuilding js doc, function to get dati from api, now works (on console.log)

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const cards = document.querySelectorAll('.card');
-const namePoke = document.querySelector('.name');
-const idPoke = document.querySelector('.id');
-const infoPoke = document.querySelector('.pokemon-info');
-const searchValue = document.querySelector('#pokemon-name');
-const buttonSearch = document.querySelector('button');
+let pokemon = document.querySelector("#pokemon-name");
+const button = document.querySelector("button");
+button.addEventListener("click", getPokeData);
+
+//Get Pokè Data from API.
+const getPokeData = () => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log("Pokemon not found", err);
+    });
+};
