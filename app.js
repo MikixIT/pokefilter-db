c87d654f61b8df7717ed042ff6009b4a4f8380c5
MikixIT <michael.46@live.it>
2023-03-09 22:22:17 +0100

    creating obj for pk and fecth edit

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const button = document.querySelector("button");
 const pokemonCount = 151;
+let pokemonInput = document.querySelector("#pokemon-name");
+
+//obj pokemon
+let pokemon = {
+  name: "charmander",
+  id: 4,
+  type: "fire",
+  weight: 8,
+  height: 6,
+};
 
 //making database all pokèmon
 
-// window.onload = {};
+window.onload = {};
 
 //Get Pokè Data from API by name.
 const getPokeData = () => {
-  fetch(`https://pokeapi.co/api/v2/${pokemon}`)
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInput.value}`)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
