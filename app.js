00026ecd982a9cc78c76d83476e33aa18fa471fa
MikixIT <michael.46@live.it>
2023-03-10 22:06:22 +0100

    functios for make db of pokè in obj "pokemons" actually something doesnt work

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
 const button = document.querySelector("button");
-const pokemonNum = 151;
+const pokemonStartNum = 151;
 let pokemonInput = document.querySelector("#pokemon-name");
 
 //obj pokemon
-let pokemon = {}; //  {1 : {"name" : "charmander", "pic" : #, "type" : ["fire"], "info" : "..."} }
-
-//making database all pokèmon
+let pokemons = {}; // formatting like -> {1 : {"name" : "charmander", "pic" : #, "type" : ["fire"], "info" : "..."} }
 
 window.onload = async () => {
-  getPokeData(1);
+  // getPokeData(1);
+  for (let i = 1; i <= pokemonStartNum; i++) {
+    await getPokeData(i);
+  }
+
+  console.log(pokemons);
+};
+
+//making database all pokèmon
+const getPokeData = async (num) => {
+  let url = `https://pokeapi.co/api/v2/pokemon/${num.toString()}`;
+  let res = await fetch(url);
+  let pokemons = await res.json();
+  console.log(pokemons);
+
+  let pokemonName = pokemons["name"];
+  let pokemonType = pokemons["types"];
+  let pokemonImg = pokemons["sprites"]["other"]["dream_world"]["front_default"];
+
+  res = await fetch(pokemons["species"]["url"]);
+  let pokemonInfo = await res.json();
+
+  // console.log(pokemonInfo);
+  pokemonInfo = pokemonInfo["flavor_text_entries"]["9"]["flavor_text"];
+  pokemons[num] = {
+    "name": pokemonName,
+    "pic": pokemonImg,
+    "types": pokemonType,
+    "info": pokemonInfo,
+  };
 };
 
 //Get Pokè Data from API by name.
-const getPokeData = () => {
+const getPokeDataName = () => {
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInput.value}`)
     .then((response) => response.json())
     .then((data) => {
