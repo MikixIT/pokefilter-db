92efe3551c3991f3d1be48877f59fafd70a58e78
MikixIT <michael.46@live.it>
2023-04-04 13:24:32 +0200

    rebuilding better js file pt.2

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const button = document.querySelector("button");
-
+let pokedex = [];
+let pokemon = {};
 let pokemonInput = document.querySelector("#pokemon-input-name");
 
+//get data from api
+
+getPokeData = () => {
+  const url = `https://pokeapi.co/api/v2/pokemon/1`;
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const pokemon = {};
+      pokemon["name"] = data.name;
+      pokemon["id"] = data.id;
+      pokemon["img"] = data.sprites.front_default;
+      pokemon["type"] = data.types.map((type) => type.type.name).join(", ");
+      console.log(pokemon);
+    })
+    .catch((err) => {
+      console.log("fetch error found!");
+    });
+};
+
+//put on obj pokemon
+
 //Get Pokè Data from API by name.
-const getPokeDataName = () => {
+const getPokeDataByName = () => {
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInput.value}`)
     .then((response) => response.json())
     .then((data) => {
@@ -14,4 +39,6 @@ const getPokeDataName = () => {
     });
 };
 
-button.addEventListener("click", getPokeDataName);
+window.onload = getPokeData();
+
+button.addEventListener("click", getPokeDataByName);
