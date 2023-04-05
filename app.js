759b4601d0908690e4ca730764121e26719505ee
MikixIT <michael.46@live.it>
2023-04-05 16:54:41 +0200

    promises for fetch pokedata

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
 const button = document.querySelector("button");
 let pokedex = [];
-let pokemon = {};
 let pokemonInput = document.querySelector("#pokemon-input-name");
 
-//get data from api
+const displayConsole = (obj) => {
+  console.log(obj);
+};
 
-getPokeData = () => {
+const getPokeData = () => {
+  const promises = [];
+
   for (let i = 1; i <= 150; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    fetch(url)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const pokemon = {
-          name: data.name,
-          id: data.id,
-          img: data.sprites.front_default,
-          type: data.types.map((type) => type.type.name).join(", "),
-        };
-        console.log(pokemon);
-      })
-      .catch((err) => {
-        console.log("fetch error found!");
-      });
+    promises.push(fetch(url).then((response) => response.json()));
   }
+
+  Promise.all(promises).then((results) => {
+    const pokemon = results.map((data) => ({
+      name: data.name,
+      id: data.id,
+      img: data.sprites["front_default"],
+      type: data.types.map((type) => type.type.name).join(", "),
+    }));
+    displayConsole(pokemon);
+  });
 };
 
 //Get Pokè Data from API by name.
@@ -42,3 +40,17 @@ const getPokeDataByName = () => {
 window.onload = getPokeData();
 
 button.addEventListener("click", getPokeDataByName);
+
+//     Promise.all()
+//       .then((response) => {response.json();})
+//       .then((data) => {
+//         const pokemon = {
+
+//         };
+//         console.log(pokemon);
+//       })
+//       .catch((err) => {
+//         console.log("fetch error found!");
+//       });
+//   }
+// };
